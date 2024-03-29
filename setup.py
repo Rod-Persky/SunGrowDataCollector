e101e698437365cf3a5b6585ee9420a3dfe47f7c
Rod Persky <770327+Rod-Persky@users.noreply.github.com>
2024-03-29 23:10:27 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='SunGrowDataCollector',
-    version="0.0.1",
+    version="0.0.2",
 
     url='https://github.com/Rod-Persky/SunGrowDataCollector',
     author='Rod Persky',
